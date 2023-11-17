5dc785711bdc0c3c8f6847c3cbfaa2c21864e809
Tony Boswell <tboswell@imodules.com>
2023-11-17 16:31:18 -0600

    Add Event Components

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,10 +1,21 @@
 import EventItem from "./event-item";
+import styles from './event-list.module.css';
 
 function EventList(props) {
     const { events } = props;
+
     return (
-        <ul>
-            {events.map(event => <EventItem />)}
+        <ul className={styles.list}>
+            {events.map(event => (
+                <EventItem 
+                    key={event.id} 
+                    id={event.id} 
+                    title={event.title} 
+                    image={event.image} 
+                    date={event.date} 
+                    location={event.location}
+                />
+            ))}
         </ul>
     )
 }
