92018388a27e545be7e9545e7dcd53037e324ec4
Tony Boswell <tboswell@imodules.com>
2023-11-29 13:59:16 -0600

    add logic for filtered event page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,40 @@
+import { useRouter } from "next/router";
+import {getFilteredEvents} from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+
 function FilteredEventsPage() {
+    const router = useRouter();
+
+    const filterData = router.query.slug;
+
+    if(!filterData) {
+        return <p className='center'>Loading...</p>;
+    }
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    // The + operator is used to convert the string values of filteredYear and filteredMonth to numbers. This is necessary because the filteredYear and filteredMonth variables are expected to contain numeric values, not strings.
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+        return <p>Invalid filter. Please adjust your values.</p>
+    }
+
+
+    const filteredEvents = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    });
+
+    if(!filteredEvents || filteredEvents.length === 0) {
+        return <p>No events found for the selected dates.</p>
+    }
+
     return (
         <div>
-            <h1>Filtered Events Page</h1>
+            <EventList  events={filteredEvents}/>
         </div>
     )
 }
