d0f621e7187cb3384109d401ee72a038a803e520
Tony Boswell <tboswell@imodules.com>
2023-11-29 14:52:57 -0600

    Add finishing touches

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,6 +1,10 @@
+import { Fragment } from "react";
 import { useRouter } from "next/router";
-import {getFilteredEvents} from "../../dummy-data";
+import { getFilteredEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from '../../components/ui/error-alert';
 
 function FilteredEventsPage() {
     const router = useRouter();
@@ -19,7 +23,16 @@ function FilteredEventsPage() {
     const numMonth = +filteredMonth;
 
     if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
-        return <p>Invalid filter. Please adjust your values.</p>
+        return (
+            <Fragment>
+                <ErrorAlert>
+                    <p>Invalid filter. Please adjust your values.</p>
+                </ErrorAlert>
+                <div className="center">
+                    <Button link="/events">Show All Events</Button>
+                </div>
+            </Fragment>
+        )
     }
 
 
@@ -29,13 +42,25 @@ function FilteredEventsPage() {
     });
 
     if(!filteredEvents || filteredEvents.length === 0) {
-        return <p>No events found for the selected dates.</p>
+        return (
+            <Fragment>
+                <ErrorAlert>
+                    <p>No events found for the selected dates.</p>
+                </ErrorAlert>
+                <div className="center">
+                    <Button link="/events">Show All Events</Button>
+                </div>
+            </Fragment>
+        )
     }
 
+    const date = new Date(numYear, numMonth - 1);
+
     return (
-        <div>
+        <Fragment>
+            <ResultsTitle date={date} />
             <EventList  events={filteredEvents}/>
-        </div>
+        </Fragment>
     )
 }
 
