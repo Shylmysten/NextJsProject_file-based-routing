48c8e45ec49b1da9d1f22c60cfa3f15bc34d8085
Tony Boswell <tboswell@imodules.com>
2023-11-29 11:50:26 -0600

    add events search component

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,8 +2,13 @@ import Link from "next/link";
 import styles from './button.module.css';
 
 function Button(props) {
+    if(props.link) {
+        return (
+            <Link className={styles.btn} href={props.link}>{props.children}</Link>
+        )
+    }
     return (
-        <Link className={styles.btn} href={props.link}>{props.children}</Link>
+        <button className={styles.btn} onClick={props.onClick}>{props.children}</button>
     )
 }
 
